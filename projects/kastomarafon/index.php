26dc17768f2868e29d2bb449025f450294ee4a50
cherkasovspace <53274183+cherkasovspace@users.noreply.github.com>
2020-07-21 16:05:33 +0300

    Update index.php

--- projects/kastomarafon/index.php
+++ projects/kastomarafon/index.php
@@ -24,43 +24,6 @@
 
 		<title>«Внутренняя кухня: PRO запуски на миллионы»</title>
 
-		<!-- Yandex.Metrika counter -->
-		<script type="text/javascript">
-		   (function (d, w, c) {
-		       (w[c] = w[c] || []).push(function() {
-		           try {
-		               w.yaCounter29880689 = new Ya.Metrika({
-		                   id:29880689,
-		                   clickmap:true,
-		                   trackLinks:true,
-		                   accurateTrackBounce:true,
-		                   webvisor:true
-		               });
-		           } catch(e) { }
-		       });
-
-		       var n = d.getElementsByTagName("script")[0],
-		           s = d.createElement("script"),
-		           f = function () { n.parentNode.insertBefore(s, n); };
-		       s.type = "text/javascript";
-		       s.async = true;
-		       s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-		       if (w.opera == "[object Opera]") {
-		           d.addEventListener("DOMContentLoaded", f, false);
-		       } else { f(); }
-		   })(document, window, "yandex_metrika_callbacks");
-		</script>
-		<noscript><div><img src="https://mc.yandex.ru/watch/29880689" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-		<!-- /Yandex.Metrika counter -->
-
-		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-5JN8TK7');</script>
-		<!-- End Google Tag Manager -->
 	</head>
 	<body>
 
@@ -876,10 +839,5 @@
 
 <!-- Форма конец -->
 
-
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JN8TK7"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 	</body>
 </html>
